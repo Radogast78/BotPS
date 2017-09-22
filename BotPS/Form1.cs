e98fd2453e94dfc29573bf85599b50a5f070f07e
Anthony <radogast78@mail.ru>
2017-09-22 15:07:04 +1000

    Добавил sendPost и sendGet

--- BotPS/Form1.cs
+++ BotPS/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,55 @@ namespace BotPS
 {
     public partial class Form1 : Form
     {
+        public CookieContainer cookies;
         public Form1()
         {
+            this.cookies = new CookieContainer();
             InitializeComponent();
+        }
+        public String sendPost(String url, String post)
+        {
+            String result = "";
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] data = encoding.GetBytes(post);
+            System.Threading.Thread.Sleep(1000);
+            HttpWebRequest postRequest = (HttpWebRequest)WebRequest.Create(url);
+            postRequest.CookieContainer = this.cookies;
+            postRequest.Method = "POST";
+            postRequest.ContentType = "application/x-www-form-urlencoded";
+            postRequest.ContentLength = data.Length;
+            Stream postStream = postRequest.GetRequestStream();
+            postStream.Write(data, 0, data.Length);
+            postStream.Close();
+            HttpWebResponse postResponse = (HttpWebResponse)postRequest.GetResponse();
+            postStream = postResponse.GetResponseStream();
+            StreamReader sr = new StreamReader(postStream);
+            result = sr.ReadToEnd();
+            sr.Close();
+            postStream.Close();
+            //cookies = postRequest.CookieContainer;
+            return result;
+        }
+        public String sendGet(String urlGet, String data)
+        {
+            String result = "";
+            System.Threading.Thread.Sleep(1000);
+            HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(urlGet);
+            getRequest.CookieContainer = cookies;
+            getRequest.Method = "GET";
+            HttpWebResponse getResponse = (HttpWebResponse)getRequest.GetResponse();
+            Stream getStream = getResponse.GetResponseStream();
+            StreamReader sr = new StreamReader(getStream);
+            result = sr.ReadToEnd();
+            sr.Close();
+            getStream.Close();
+            return result;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
